6525c862ce0e1744c9e55d179f1a635c5073aa41
Gary Mathews <contact@garymathews.com>
2025-11-05 08:56:15 -0800

    update user agent to chrome on macOS

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
 // shadowharvester/src/constants.rs
 
 // UserAgent String
-pub const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36";
+pub const USER_AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36";
